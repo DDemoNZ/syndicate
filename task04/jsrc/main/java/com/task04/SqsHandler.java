bddc475e412f9cf6fd214535e8cb2cffc6251c5a
Dmytro Zinchuk1 <dmytro_zinchuk1@epam.com>
2024-01-26 10:43:44 +0200

    try to fix

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -6,6 +6,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +21,7 @@ import java.util.stream.Collectors;
         targetQueue = "async_queue",
         batchSize = 10
 )
+@DependsOn(resourceType = ResourceType.SNS_TOPIC, name = "async_queue")
 public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 
     public Void handleRequest(SQSEvent request, Context context) {
