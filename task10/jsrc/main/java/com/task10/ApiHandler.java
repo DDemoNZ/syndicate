ba95f8f34f7f7f283b9cad7ff49d7456b089b9ee
Dmytro Zinchuk1 <dmytro_zinchuk1@epam.com>
2024-02-13 14:48:58 +0200

    minor

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -1,7 +1,5 @@
 package com.task10;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
