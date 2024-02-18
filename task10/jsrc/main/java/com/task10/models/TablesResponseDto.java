7a516cbc0a98d9918a73c83917e5d26fc44bf12e
Dmytro Zinchuk1 <dmytro_zinchuk1@epam.com>
2024-02-18 23:16:08 +0200

    m

--- task10/jsrc/main/java/com/task10/models/TablesResponseDto.java
+++ task10/jsrc/main/java/com/task10/models/TablesResponseDto.java
@@ -1,6 +1,5 @@
 package com.task10.models;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
@@ -8,9 +7,6 @@ import java.util.List;
 @Data
 public class TablesResponseDto {
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private int id;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<Table> tables;
 
 }
