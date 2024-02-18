d798ef671f0d10cca599a4ea4e740d15eac9c310
Dmytro Zinchuk1 <dmytro_zinchuk1@epam.com>
2024-02-18 22:38:10 +0200

    m

--- task10/jsrc/main/java/com/task10/models/Table.java
+++ task10/jsrc/main/java/com/task10/models/Table.java
@@ -1,28 +1,20 @@
 package com.task10.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 public class Table {
+
     private int id;
     private int number;
     private int places;
+    @JsonProperty("isVip")
     private boolean isVip;
     private int minOrder;
 
-    public boolean isVip() {
-        return isVip;
-    }
-
-    public boolean getVip() {
-        return isVip;
-    }
-
-    public void setVip(boolean vip) {
-        isVip = vip;
-    }
 }
