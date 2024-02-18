c425f32b2c717a715f820ff437f9ceceaeed875c
Dmytro Zinchuk1 <dmytro_zinchuk1@epam.com>
2024-02-18 22:14:41 +0200

    m

--- task10/jsrc/main/java/com/task10/models/Table.java
+++ task10/jsrc/main/java/com/task10/models/Table.java
@@ -1,8 +1,12 @@
 package com.task10.models;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class Table {
     private int id;
     private int number;
