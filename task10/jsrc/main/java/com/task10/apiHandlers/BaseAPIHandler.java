672507ab8abde376f49a1cf087b2fe049d5c3374
Dmytro Zinchuk1 <dmytro_zinchuk1@epam.com>
2024-02-19 12:37:03 +0200

    m

--- task10/jsrc/main/java/com/task10/apiHandlers/BaseAPIHandler.java
+++ task10/jsrc/main/java/com/task10/apiHandlers/BaseAPIHandler.java
@@ -3,6 +3,8 @@ package com.task10.apiHandlers;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
+import java.util.Map;
+
 public interface BaseAPIHandler {
 
     String HTTP_METHOD_GET = "GET";
@@ -12,11 +14,17 @@ public interface BaseAPIHandler {
         if (HTTP_METHOD_GET.equals(requestEvent.getHttpMethod())) {
             System.out.println(getClass() + "requestEvent.getResource() " + requestEvent.getResource());
             System.out.println(getClass() + "requestEvent.getHttpMethod() " + requestEvent.getHttpMethod());
+            System.out.println(getClass() + "requestEvent.getHttpMethod() " + requestEvent.getPath());
+            System.out.println(getClass() + " request with attributes " + requestEvent.getPath().matches("/\\d+"));
             if (requestEvent.getPath().matches("/\\d+")) {
-                System.out.println(getClass() + "attributes");
+                System.out.println(getClass() + " request with attributes");
+                Map<String, String> attributesMap = requestEvent.getPathParameters();
+                System.out.println(getClass() + " attributes " + attributesMap);
                 return handleGetWithAttributes(requestEvent);
+            } else {
+                System.out.println(getClass() + " usual get request");
+                return handleGet(requestEvent);
             }
-            return handleGet(requestEvent);
         } else if (HTTP_METHOD_POST.equals(requestEvent.getHttpMethod())) {
             return handlePost(requestEvent);
         } else {
