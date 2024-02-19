786e7586b89c2ccc181ba7c80233973e79aa9cde
Dmytro Zinchuk1 <dmytro_zinchuk1@epam.com>
2024-02-19 23:36:14 +0200

    m

--- task10/jsrc/main/java/com/task10/apiHandlers/HandlerReservation.java
+++ task10/jsrc/main/java/com/task10/apiHandlers/HandlerReservation.java
@@ -7,11 +7,14 @@ import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.task10.models.ReservationsRequestDto;
 import com.task10.models.ReservationResponseDto;
+import com.task10.models.ReservationsRequestDto;
 import com.task10.utils.DynamoDBUtils;
 import lombok.Data;
+import org.apache.http.HttpStatus;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +26,13 @@ import static com.task10.utils.Constants.*;
 @Data
 public class HandlerReservation implements BaseAPIHandler {
 
+    private static final String TABLE_NUMBER = "tableNumber";
+    private static final String CLIENT_NAME = "clientName";
+    private static final String PHONE_NUMBER = "phoneNumber";
+    private static final String DATE = "date";
+    private static final String SLOT_TIME_START = "slotTimeStart";
+    private static final String SLOT_TIME_END = "slotTimeEnd";
+    private static final String ID = "id";
     private String path = "reservations";
     private AmazonDynamoDB client = DynamoDBUtils.getAmazonDynamoDBClient();
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -33,9 +43,10 @@ public class HandlerReservation implements BaseAPIHandler {
 //            CognitoUtils.authenticateUser(event);
             ReservationsRequestDto reservationsRequestDto = objectMapper.readValue(event.getBody(), ReservationsRequestDto.class);
             reservationsRequestDto.setId(UUID.randomUUID().toString());
-            System.out.println(getClass()+ " 40 reservationsDto " + reservationsRequestDto);
+            System.out.println(getClass() + " 40 reservationsDto " + reservationsRequestDto);
             Map<String, AttributeValue> reservationPutItem = getReservationPutItem(reservationsRequestDto);
             System.out.println(getClass() + " 42 reservationPutItem " + reservationPutItem);
+            checkReservationOverlap(reservationsRequestDto);
             PutItemResult putItemResult = client.putItem(PREFIX + RESERVATIONS_TABLE_NAME + SUFFIX, reservationPutItem);
             System.out.println(getClass() + " 44 putItemResult " + putItemResult);
             ReservationResponseDto reservationResponseDto = new ReservationResponseDto();
@@ -44,9 +55,10 @@ public class HandlerReservation implements BaseAPIHandler {
             return new APIGatewayProxyResponseEvent().withBody(objectMapper.writeValueAsString(reservationResponseDto));
         } catch (Exception e) {
             System.out.println(getClass() + " 51 " + e.getMessage());
-            return new APIGatewayProxyResponseEvent();
+            return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatus.SC_BAD_REQUEST);
         }
     }
+
 
     @Override
     public APIGatewayProxyResponseEvent handleGet(APIGatewayProxyRequestEvent event) {
@@ -75,13 +87,13 @@ public class HandlerReservation implements BaseAPIHandler {
     private Map<String, AttributeValue> getReservationPutItem(ReservationsRequestDto reservationsRequestDto) {
         System.out.println(getClass() + " 81 reservationsDto " + reservationsRequestDto);
         HashMap<String, AttributeValue> reservationItem = new HashMap<>();
-        reservationItem.put("id", new AttributeValue().withS(reservationsRequestDto.getId()));
-        reservationItem.put("tableNumber", new AttributeValue().withN(String.valueOf(reservationsRequestDto.getTableNumber())));
-        reservationItem.put("clientName", new AttributeValue().withS(reservationsRequestDto.getClientName()));
-        reservationItem.put("phoneNumber", new AttributeValue().withS(reservationsRequestDto.getPhoneNumber()));
-        reservationItem.put("date", new AttributeValue().withS(reservationsRequestDto.getDate()));
-        reservationItem.put("slotTimeStart", new AttributeValue().withS(reservationsRequestDto.getSlotTimeStart()));
-        reservationItem.put("slotTimeEnd", new AttributeValue().withS(reservationsRequestDto.getSlotTimeEnd()));
+        reservationItem.put(ID, new AttributeValue().withS(reservationsRequestDto.getId()));
+        reservationItem.put(TABLE_NUMBER, new AttributeValue().withN(String.valueOf(reservationsRequestDto.getTableNumber())));
+        reservationItem.put(CLIENT_NAME, new AttributeValue().withS(reservationsRequestDto.getClientName()));
+        reservationItem.put(PHONE_NUMBER, new AttributeValue().withS(reservationsRequestDto.getPhoneNumber()));
+        reservationItem.put(DATE, new AttributeValue().withS(reservationsRequestDto.getDate()));
+        reservationItem.put(SLOT_TIME_START, new AttributeValue().withS(reservationsRequestDto.getSlotTimeStart()));
+        reservationItem.put(SLOT_TIME_END, new AttributeValue().withS(reservationsRequestDto.getSlotTimeEnd()));
         System.out.println(getClass() + " 90 reservationItem " + reservationItem);
         return reservationItem;
     }
@@ -94,14 +106,55 @@ public class HandlerReservation implements BaseAPIHandler {
 
     private ReservationsRequestDto mapFromItemToReservation(Map<String, AttributeValue> reservation) {
         ReservationsRequestDto reservationsRequestDto = new ReservationsRequestDto();
-        reservationsRequestDto.setId(reservation.get("id").getS());
-        reservationsRequestDto.setTableNumber(Integer.parseInt(reservation.get("tableNumber").getN()));
-        reservationsRequestDto.setClientName(reservation.get("clientName").getS());
-        reservationsRequestDto.setPhoneNumber(reservation.get("phoneNumber").getS());
-        reservationsRequestDto.setDate(reservation.get("date").getS());
-        reservationsRequestDto.setSlotTimeStart(reservation.get("slotTimeStart").getS());
-        reservationsRequestDto.setSlotTimeEnd(reservation.get("slotTimeEnd").getS());
+        reservationsRequestDto.setId(reservation.get(ID).getS());
+        reservationsRequestDto.setTableNumber(Integer.parseInt(reservation.get(TABLE_NUMBER).getN()));
+        reservationsRequestDto.setClientName(reservation.get(CLIENT_NAME).getS());
+        reservationsRequestDto.setPhoneNumber(reservation.get(PHONE_NUMBER).getS());
+        reservationsRequestDto.setDate(reservation.get(DATE).getS());
+        reservationsRequestDto.setSlotTimeStart(reservation.get(SLOT_TIME_START).getS());
+        reservationsRequestDto.setSlotTimeEnd(reservation.get(SLOT_TIME_END).getS());
         System.out.println(getClass() + " reservationsDto " + reservationsRequestDto);
+        return reservationsRequestDto;
+    }
+
+
+    private void checkReservationOverlap(ReservationsRequestDto newReservation) {
+        client.scan(new ScanRequest()
+                        .withTableName(PREFIX + COGNITO_POOL_NAME + SUFFIX))
+                .getItems().stream()
+                .map(this::mapToReservation)
+                .filter(reservation -> checkOverlap(reservation, newReservation))
+                .findAny()
+                .ifPresent(reservation -> {
+                    throw new IllegalArgumentException("Overlap found with existing reservation: " + reservation);
+                });
+    }
+
+    private boolean checkOverlap(ReservationsRequestDto reservation, ReservationsRequestDto newReservation) {
+        LocalTime storedStartTime = LocalTime.parse(reservation.getSlotTimeStart());
+        LocalDate storedStartDate = LocalDate.parse(reservation.getDate());
+        LocalTime storedEndTime = LocalTime.parse(reservation.getSlotTimeEnd());
+
+        LocalTime newStartTime = LocalTime.parse(newReservation.getSlotTimeStart());
+        LocalDate newStartDate = LocalDate.parse(newReservation.getDate());
+        LocalTime newEndTime = LocalTime.parse(newReservation.getSlotTimeEnd());
+
+        System.out.println(getClass() + " 142 stored " + reservation);
+        System.out.println(getClass() + " 143 new " + newReservation);
+        return (storedStartDate.isAfter(newStartDate) || storedStartDate.isEqual(newStartDate))
+                && (storedStartTime.isAfter(newStartTime) || storedStartTime.equals(newStartTime))
+                && (storedEndTime.isBefore(newEndTime) || storedEndTime.equals(newEndTime));
+    }
+
+    private ReservationsRequestDto mapToReservation(Map<String, AttributeValue> reservationAttributeValueMap) {
+        ReservationsRequestDto reservationsRequestDto = new ReservationsRequestDto();
+        reservationsRequestDto.setId(reservationAttributeValueMap.get(ID).getS());
+        reservationsRequestDto.setTableNumber(Integer.parseInt(reservationAttributeValueMap.get(TABLE_NUMBER).getN()));
+        reservationsRequestDto.setClientName(reservationAttributeValueMap.get(CLIENT_NAME).getS());
+        reservationsRequestDto.setPhoneNumber(reservationAttributeValueMap.get(PHONE_NUMBER).getS());
+        reservationsRequestDto.setDate(reservationAttributeValueMap.get(DATE).getS());
+        reservationsRequestDto.setSlotTimeStart(reservationAttributeValueMap.get(SLOT_TIME_START).getS());
+        reservationsRequestDto.setSlotTimeEnd(reservationAttributeValueMap.get(SLOT_TIME_END).getS());
         return reservationsRequestDto;
     }
 }
